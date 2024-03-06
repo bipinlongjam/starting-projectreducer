153e37a24cb87ae6c8f5b82f6ae6e1c3bbf5b949
bipinlongjam <bipinlongjam11@gmail.com>
2024-03-07 02:33:53 +0530

    send and fetch data

--- src/App.js
+++ src/App.js
@@ -3,48 +3,34 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useEffect } from 'react';
-import { uiActions } from './store/ui-slice';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
 
 function App() {
 
   const dispatch = useDispatch()
   const showCart= useSelector(state => state.ui.cartIsVisible)
   const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification)
+
+  let isInitial = true;
+
+  useEffect(()=>{
+    dispatch(fetchCartData)
+  },[dispatch])
 
   useEffect(() =>{
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status:'pending',
-        title:'Sending...',
-        message:'Sending cart data!'
-      }))
-      const response = await fetch('https://react-ecom-e69f7-default-rtdb.firebaseio.com/cart.json',{
-      method:'PUT',
-      body:JSON.stringify(cart),
-    })
-    if(!response.ok){
-      throw new Error('Sending cart data failed')
+    if(isInitial){
+      isInitial = false
+      return;
     }
-    //const responseData = await response.json();
-
-    dispatch(uiActions.showNotification({
-      status:'success',
-      title:'Success',
-      message:'Sending cart data successfully'
-    }))
-  }
-  sendCartData().catch((error) =>{
-    dispatch(uiActions.showNotification({
-      status:'error',
-      title:'error',
-      message:'Sending cart data failed'
-    }))
-  })
+    if(cart.changed){
+      dispatch(sendCartData(cart))
+    }
   },[cart, dispatch])
   return (
     <Layout>
       {showCart && <Cart />}
-      
       <Products />
     </Layout>
   );
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
     name:'cart',
     initialState:{
         items:[],
         totalQuantity:0,
+        changed:false
     },
     reducers:{
+        replaceCart(state, action){
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
         addItemToCart(state, action){
             const newItem = action.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
             state.totalQuantity++;
+            state.changed = true;
             if(!existingItem){
                 state.items.push({
                     id: newItem.id,
